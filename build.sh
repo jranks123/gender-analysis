5e176f296814f1164097ff2b46cb22e5d34dcb63
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2019-08-18 10:59:04 -0400

    first commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+NODE_VERSION="8.10"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm use ${NODE_VERSION}
+
+npm install
+npm run riffraff-artefact
